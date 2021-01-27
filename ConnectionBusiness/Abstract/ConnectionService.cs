f48b6a91b3556dd82283eb86e1c2ef76432906e7
Ali <ali.mahmoodi.heri@gmail.com>
2021-01-27 20:51:27 +0300

    Add ConnectionService The job of this service is to manage clients

---
+++ ConnectionBusiness/Abstract/ConnectionService.cs
@@ -0,0 +1,22 @@
+﻿
+using Core.Utilities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConnectionBusiness.Abstract
+{
+    public interface ConnectionService
+    {
+        IDataResult<List<Client>> Get();
+        IDataResult<Client> GetById(Guid id);
+        IDataResult<Client> GetByGroup(Guid groupId);
+        IDataResult<List<Client>> GetOnline();
+        IDataResult<List<Client>> GetOffline();
+        IDataResult<Client> Add(Client client);
+        IResult AddRange(List<Client> clients);
+        IResult Delete(Client client);
+        IResult Update(Client client);
+
+    }
+}
